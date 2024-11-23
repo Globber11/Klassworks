e09d1c9868baf85fccf5fff1c1d2b2926a2845b0
ViperrIvan <smokerskatedik@gmail.com>
2024-11-23 19:09:14 +0700

    Update view.py

--- Pizza/view.py
+++ Pizza/view.py
@@ -11,7 +11,7 @@ def menu(user_input):
         6: 'сок'
     }
     return products[user_input]
-  def menu18(user_input):
+def menu18(user_input):
     global products
     products = {
         1: 'пепперони',
@@ -23,14 +23,34 @@ def menu(user_input):
         7: 'кальянчик'
     }
     return products[user_input]
-  def printMenu():
+def printMenu():
     print('Меню: ')
     for i in range(1, 6):
         print(f'   {i}){menu(i)}: {cost(menu(i))}')
-  def printMenu():
+def printMenu18():
     print('Меню: ')
     for i in range(1, 7):
-        print(f'   {i}){menu18(i)}: {cost(menu18(i))}')
+        print(f'   {i}){menu18(i)}: {cost18(menu18(i))}')
+def cost(key):
+    cost = {
+        'пепперони': 50,
+        'маргарита': 100,
+        'четыре сыра':200,
+        'ветчина и грибы':250,
+        'минипицца': 300,
+        'сок': 350
+    }
+def cost18(key):
+    cost = {
+        'пепперони': 50,
+        'маргарита': 100,
+        'четыре сыра':200,
+        'ветчина и грибы':250,
+        'пиво': 300,
+        'виски с колой': 350,
+        'кальянчик': 500
+    }
+    return cost[key]
 if 2024-user_data[born_year]>=18:
     printMenu18()
 else:
