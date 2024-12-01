6d502b957c6bc80f831aaab343528207792f4a6a
ViperrIvan <smokerskatedik@gmail.com>
2024-12-01 17:17:51 +0700

    Update controller.py

--- Pizza/controller.py
+++ Pizza/controller.py
@@ -1,8 +1,10 @@
+import sys
 from view import print_menu
 from model import busketSave
 from model import reg_and_create_id
 from view import buy
 from view import check
+from model import craftPizza
 
 busket = []
 def zakaz(bornn_year):
@@ -11,12 +13,19 @@ def zakaz(bornn_year):
     try:
         print('Что вы хотели бы заказать?')
         user_input = int(input('Введите цифру соответствующую ващему выбору: '))
-        user_number = int(input('введите количество: '))
-        busketSave(busket, user_input, user_number, bornn_year)
+        if user_input == 8 and 2024-bornn_year>=18:
+            craftPizza()
+        elif user_input == 7 and 2024-bornn_year<18:
+            craftPizza()
+        else:
+            user_number = int(input('введите количество: '))
+        busketSave(busket, user_input, bornn_year, user_number)
         if input('Для продолжения заказа введите 1, иначе любой символ: ')=='1':
             zakaz(bornn_year)
         else:
             check(busket, buy(busket))
+            print('♥♥♥Спасибо за заказ!♥♥♥\n   Приходите еще)')
+            sys.exit()
     except KeyError:
         print('Такого варианта нет!!!')
         zakaz(bornn_year)
