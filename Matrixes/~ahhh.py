c406fbff166562f53efe17f3507a159ae0b70539
Globber11 <kg05052011@gmail.com>
2025-01-18 16:35:28 +0700

    Add files via upload

--- Matrixes/~ahhh.py
+++ Matrixes/~ahhh.py
@@ -2,7 +2,7 @@ import math
 
 matrix_size = int(input('Введите размер матрицы: '))
 
-matrix = [[1 for _ in range(matrix_size)] for _ in range(matrix_size)]
+matrix = [['' for _ in range(matrix_size)] for _ in range(matrix_size)]
 
 def paint_circle_in_matrix(matrix, size=None):
     if size is not None:
@@ -17,8 +17,12 @@ def paint_circle_in_matrix(matrix, size=None):
     for y in range(rows):
         for x in range(cols):
             distance = math.sqrt((x - center_x) ** 2 + (y - center_y) ** 2)
-            if distance <= radius:
+            if round(distance) == radius:
                 matrix[y][x] = 0
+            elif x == 0 or x == cols - 1 or y == 0 or y == rows - 1:
+                matrix[y][x] = 1
+            else:
+                matrix[y][x] = ' '
     return matrix
 
 for row in paint_circle_in_matrix(matrix, int(input('Введите размер круга: '))):
